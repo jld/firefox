jld/gecko-dev

Bug 1752638 - Part 2: remote `waitpid`/`SIGCHLD` to/from the fork server. r?#ipc-reviewers
There are two parts to this:

1. A new sync IPC call to the fork server which is basically just
waitpid: it takes a pid and a flag for whether to block, and returns
either the exit status, an error, or an indication that the process is
still running.  The code in the parent process which would previously
hack around the fork server case with `kill(pid, 0)` now uses this
instead.

2. The fork server now has a `SIGCHLD` handler which writes to a pipe
given at startup.  Currently this is connected to the same pipe-to-self
used in `ProcessWatcher` for the parents' own `SIGCHLD`s.  Thus, the
existing `ProcessWatcher` is mostly unmodified.

Measured locally, these remote `waitpid`s take about 50µs, compared to
about 10µs for in-process.  So, while it's possible for the callback
for `SIGCHLD` pipe reads to call this `O(n)` times when `n` child
processes are exiting at once, the time taken shouldn't be a significant
responsiveness problem; I measured 250µs max while closing a tab for a
popular website with a large number of out-of-process iframes.  This can
be optimized later to batch the messages if needed.

The other potential performance issue is if a remote `waitpid` is blocked
behind a remote fork.  However, this is still an improvement over the
non-forkserver status quo: if we `clone` the parent process, that blocks
all threads for, empirically, about 15ms; cloning the fork server is
about 2.5ms, and that would only block another thread if it happens to
race with another child process exiting and not every time.  We also
prelaunch content processes during idle time, so this type of jank is
less likely to be user-visible in any case.

Importantly, the way this patch works, the `waitpid` is synchronous with
respect to the thread in the parent process making the requeste, so as
long as the fork server hasn't crashed then we can know that the process
with a given pid is in fact the 

ipc/glue/ForkServer.h
@@ -1,5 +1,5 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/* vim: set ts=8 sts=4 et sw=4 tw=80: */
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
@@ -7,6 +7,7 @@
 #define __FORKSERVER_H_
 
 #include "mozilla/UniquePtr.h"
+#include "mozilla/UniquePtrExtensions.h"
 #include "base/process_util.h"
 #include "mozilla/ipc/MiniTransceiver.h"
 
@@ -19,10 +20,6 @@ class ForkServer {
   ~ForkServer() = default;
 
   void InitProcess(int* aArgc, char*** aArgv);
-  bool HandleMessages();
-
-  // Called when a message is received.
-  bool OnMessageReceived(UniquePtr<IPC::Message> message);
 
   static bool RunForkServer(int* aArgc, char*** aArgv);
 
@@ -31,12 +28,18 @@ class ForkServer {
 
   int* mArgc;
   char*** mArgv;
+
+  bool HandleMessages();
+  bool HandleForkNewSubprocess(UniquePtr<IPC::Message> message);
+  void HandleWaitPid(UniquePtr<IPC::Message> message);
 };
 
 enum {
   Msg_ForkNewSubprocess__ID = 0x7f0,  // a random picked number
   Reply_ForkNewSubprocess__ID,
   Msg_SubprocessExecInfo__ID,
+  Msg_WaitPid__ID,
+  Reply_WaitPid__ID,
 };
 
 }  // namespace ipc

jld/firefox

ipc/glue/ForkServer.h
@@ -1,5 +1,5 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/* vim: set ts=8 sts=4 et sw=4 tw=80: */
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
@@ -7,6 +7,7 @@
 #define __FORKSERVER_H_
 
 #include "mozilla/UniquePtr.h"
+#include "mozilla/UniquePtrExtensions.h"
 #include "base/process_util.h"
 #include "mozilla/ipc/MiniTransceiver.h"
 
@@ -19,14 +20,14 @@ class ForkServer {
   ~ForkServer() = default;
 
   void InitProcess(int* aArgc, char*** aArgv);
-  bool HandleMessages();
-
-  // Called when a message is received.
-  bool OnMessageReceived(UniquePtr<IPC::Message> message);
 
   static bool RunForkServer(int* aArgc, char*** aArgv);
 
  private:
+  bool HandleMessages();
+  bool HandleForkNewSubprocess(UniquePtr<IPC::Message> aMessage);
+  void HandleWaitPid(UniquePtr<IPC::Message> aMessage);
+
   UniqueFileHandle mIpcFd;
   UniquePtr<MiniTransceiver> mTcver;
 
@@ -38,6 +39,8 @@ enum {
   Msg_ForkNewSubprocess__ID = 0x7f0,  // a random picked number
   Reply_ForkNewSubprocess__ID,
   Msg_SubprocessExecInfo__ID,
+  Msg_WaitPid__ID,
+  Reply_WaitPid__ID,
 };
 
 }  // namespace ipc